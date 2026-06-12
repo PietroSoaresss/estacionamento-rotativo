8cbd778f338a8bc3fcb1b7e060063ae5ec92608a
Pietro Soares <soarespietro19@gmail.com>
2026-06-11 21:00:17 -0300

    chore: classe carro

---
+++ src/main/java/models/Veiculo.java
@@ -0,0 +1,11 @@
+package models;
+
+import java.time.LocalDate;
+
+public abstract class Veiculo {
+    private String modelo;
+    private String ano;
+    private String placa;
+    private LocalDate horaEntrada;
+
+}
